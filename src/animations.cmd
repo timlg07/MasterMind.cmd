c998d6b8635689bcd24bfa7080e085d7b47d936d
Tim Greller <timlg07@gmail.com>
2020-04-11 16:39:32 +0200

    changed animation text

--- src/animations.cmd
+++ src/animations.cmd
@@ -2,7 +2,7 @@ goto %~1Animation
 exit /b 1
 
 :startAnimation
-	batbox /g 6  3 /c 0x0c /d "MASTERMINDbatch"   /a 184 /w 200
+	batbox /g 6  3 /c 0x0c /d "MASTERMIND batch"         /w 200
 	batbox /g 6  3 /c 0x04 /d "M" /g 7  3 /c 0x0f /d "A" /w 50
 	batbox /g 7  3 /c 0x04 /d "A" /g 8  3 /c 0x0f /d "S" /w 50
 	batbox /g 8  3 /c 0x04 /d "S" /g 9  3 /c 0x0f /d "T" /w 50
@@ -12,16 +12,16 @@ exit /b 1
 	batbox /g 12 3 /c 0x04 /d "M" /g 13 3 /c 0x0f /d "I" /w 50
 	batbox /g 13 3 /c 0x04 /d "I" /g 14 3 /c 0x0f /d "N" /w 50
 	batbox /g 14 3 /c 0x04 /d "N" /g 15 3 /c 0x0f /d "D" /w 50
-	batbox /g 15 3 /c 0x04 /d "D" /g 16 3 /c 0x0f /d "b" /w 50
-	batbox /g 16 3 /c 0x04 /d "b" /g 17 3 /c 0x0f /d "a" /w 50
-	batbox /g 17 3 /c 0x04 /d "a" /g 18 3 /c 0x0f /d "t" /w 50
-	batbox /g 18 3 /c 0x04 /d "t" /g 19 3 /c 0x0f /d "c" /w 50
-	batbox /g 19 3 /c 0x04 /d "c" /g 20 3 /c 0x0f /d "h" /w 50
-	batbox /g 20 3 /c 0x04 /d "h" /g 21 3 /c 0x0f /a 184 /w 200
-	batbox /g 6  3 /c 0x04 /d "MASTERMINDbatch" /a 184 /w 200 
-	batbox /g 6  3 /c 0x0c /d "MASTERMINDbatch" /a 184 /w 200 
-	batbox /g 6  3 /c 0x04 /d "MASTERMINDbatch" /a 184 /w 200 
-	batbox /g 6  3 /c 0x0c /d "MASTERMINDbatch" /a 184 /w 200 
+	batbox /g 15 3 /c 0x04 /d "D" /g 16 3 /c 0x0f /d " " /w 50
+	batbox /g 16 3 /c 0x04 /d " " /g 17 3 /c 0x0f /d "b" /w 50
+	batbox /g 17 3 /c 0x04 /d "b" /g 18 3 /c 0x0f /d "a" /w 50
+	batbox /g 18 3 /c 0x04 /d "a" /g 19 3 /c 0x0f /d "t" /w 50
+	batbox /g 19 3 /c 0x04 /d "t" /g 20 3 /c 0x0f /d "c" /w 50
+	batbox /g 20 3 /c 0x04 /d "c" /g 21 3 /c 0x0f /d "h" /w 200
+	batbox /g 6  3 /c 0x04 /d "MASTERMIND batch"         /w 200 
+	batbox /g 6  3 /c 0x0c /d "MASTERMIND batch"         /w 200 
+	batbox /g 6  3 /c 0x04 /d "MASTERMIND batch"         /w 200 
+	batbox /g 6  3 /c 0x0c /d "MASTERMIND batch"         /w 200 
 exit /b 0
 
 :checkButtonAnimation
